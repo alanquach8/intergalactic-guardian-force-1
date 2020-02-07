920375ed6b69e67c3bbdc16764f5ef05ea4573d8
Alan Quach <alanquach2000@hotmail.com>
2020-02-07 16:19:50 -0500

    player able to move/rotate with arrow keys

--- Scripts/objects/Player.js
+++ Scripts/objects/Player.js
@@ -16,14 +16,100 @@ var objects;
 (function (objects) {
     var Player = /** @class */ (function (_super) {
         __extends(Player, _super);
-        // PRIVATE INSTANCE MEMBERS
-        // PUBLIC PROPERTIES
         // CONSTRUCTOR
         function Player() {
             var _this = _super.call(this, "./Assets/images/placeholder.png", 0, 0, true) || this;
+            _this._speed = 1;
+            _this._rotate = 1; // degrees
+            _this._facing = 270; // initially looking up (-90degrees on canvas axis = 270degrees on normal axis)
+            _this._direction = new objects.Vector2(Math.cos(_this.facing * Math.PI / 180), Math.sin(_this.facing * Math.PI / 180));
+            window.addEventListener('keydown', function (e) {
+                switch (e.code) {
+                    case "ArrowUp":
+                        _this.y += _this.direction.y * _this.speed;
+                        _this.x += _this.direction.x * _this.speed;
+                        break;
+                    case "ArrowDown":
+                        _this.y -= _this.direction.y * _this.speed;
+                        _this.x -= _this.direction.x * _this.speed;
+                        break;
+                    case "ArrowRight":
+                        _this.rotation += _this.rotate;
+                        _this.facing += _this.rotate;
+                        _this._direction = new objects.Vector2(Math.cos(_this.facing * Math.PI / 180), Math.sin(_this.facing * Math.PI / 180));
+                        break;
+                    case "ArrowLeft":
+                        _this.rotation -= _this.rotate;
+                        _this.facing -= _this.rotate;
+                        _this._direction = new objects.Vector2(Math.cos(_this.facing * Math.PI / 180), Math.sin(_this.facing * Math.PI / 180));
+                        break;
+                    default:
+                        break;
+                }
+                console.log("x:" + _this.direction.x + ", y:" + _this.direction.y);
+                // window.addEventListener('keydown', (e) => {
+                //     switch(e.keyCode) {
+                //         case 38: //ArrowUp
+                //             player.y -= 10;
+                //             break;
+                //         case 40: //ArrowDown
+                //             player.y += 10;
+                //             break;
+                //         case 39: //ArrowRight
+                //             player.rotation += 1;
+                //             break;
+                //         case 37: //ArrowLeft
+                //             player.rotation -= 1;
+                //             break;
+                //         default:
+                //             break;
+                //     }
+                // });
+            });
             _this.Start();
             return _this;
         }
+        Object.defineProperty(Player.prototype, "direction", {
+            // PUBLIC PROPERTIES
+            get: function () {
+                return this._direction;
+            },
+            set: function (newDirection) {
+                this._direction = newDirection;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Player.prototype, "speed", {
+            get: function () {
+                return this._speed;
+            },
+            set: function (newSpeed) {
+                this._speed = newSpeed;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Player.prototype, "facing", {
+            get: function () {
+                return this._facing;
+            },
+            set: function (newFacing) {
+                this._facing = newFacing;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Player.prototype, "rotate", {
+            get: function () {
+                return this._rotate;
+            },
+            set: function (newRotate) {
+                this._rotate = newRotate;
+            },
+            enumerable: true,
+            configurable: true
+        });
         // PRIVATE METHODS
         Player.prototype._checkBounds = function () {
         };
@@ -31,7 +117,6 @@ var objects;
         Player.prototype.Start = function () {
         };
         Player.prototype.Update = function () {
-            this.position = new objects.Vector2(this.stage.mouseX, this.stage.mouseY);
         };
         Player.prototype.Reset = function () {
         };
--- Scripts/objects/Player.ts
+++ Scripts/objects/Player.ts
@@ -3,13 +3,96 @@ module objects {
     export class Player extends GameObject {
             
         // PRIVATE INSTANCE MEMBERS
+        private _direction:Vector2;
+        private _speed:number = 1;
+        private _facing:number;
+        private _rotate:number = 1; // degrees
 
         // PUBLIC PROPERTIES
+        get direction():Vector2 {
+            return this._direction;
+        }
+
+        set direction(newDirection:Vector2) {
+            this._direction = newDirection;
+        }
+
+        get speed():number {
+            return this._speed
+        }
+
+        set speed(newSpeed:number) {
+            this._speed = newSpeed;
+        }
+
+        get facing():number {
+            return this._facing;
+        }
+
+        set facing(newFacing:number) {
+            this._facing = newFacing;
+        }
+
+        get rotate():number {
+            return this._rotate;
+        }
+
+        set rotate(newRotate:number) {
+            this._rotate = newRotate;
+        }
 
         // CONSTRUCTOR
         constructor()
         {
             super("./Assets/images/placeholder.png", 0, 0, true);
+            
+            this._facing = 270; // initially looking up (-90degrees on canvas axis = 270degrees on normal axis)
+            this._direction = new Vector2(Math.cos(this.facing*Math.PI/180),Math.sin(this.facing*Math.PI/180));
+
+            window.addEventListener('keydown', (e) => {
+                switch(e.code) {
+                    case "ArrowUp":
+                        this.y += this.direction.y * this.speed;
+                        this.x += this.direction.x * this.speed;
+                        break;
+                    case "ArrowDown":
+                        this.y -= this.direction.y * this.speed;
+                        this.x -= this.direction.x * this.speed;
+                        break;
+                    case "ArrowRight":
+                        this.rotation += this.rotate;
+                        this.facing += this.rotate;
+                        this._direction = new Vector2(Math.cos(this.facing*Math.PI/180),Math.sin(this.facing*Math.PI/180));
+                        break;
+                    case "ArrowLeft":
+                        this.rotation -= this.rotate;
+                        this.facing -= this.rotate;
+                        this._direction = new Vector2(Math.cos(this.facing*Math.PI/180),Math.sin(this.facing*Math.PI/180));
+                        break;
+                    default:
+                        break;
+                }
+                console.log("x:" + this.direction.x + ", y:" + this.direction.y);
+
+                // window.addEventListener('keydown', (e) => {
+                //     switch(e.keyCode) {
+                //         case 38: //ArrowUp
+                //             player.y -= 10;
+                //             break;
+                //         case 40: //ArrowDown
+                //             player.y += 10;
+                //             break;
+                //         case 39: //ArrowRight
+                //             player.rotation += 1;
+                //             break;
+                //         case 37: //ArrowLeft
+                //             player.rotation -= 1;
+                //             break;
+                //         default:
+                //             break;
+                //     }
+                // });
+            });
 
             this.Start();
         }
@@ -24,7 +107,7 @@ module objects {
             
         }
         public Update(): void {
-            this.position = new Vector2(this.stage.mouseX, this.stage.mouseY);
+            
         }
         public Reset(): void {
             
