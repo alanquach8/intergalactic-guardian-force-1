d1291e7425ef779c81cc3de7632606922aded835
Kissy1999 <35816345+Kissy1999@users.noreply.github.com>
2020-02-09 12:45:27 -0500

    Add files via upload

---
+++ Scripts/objects/Bullet.js
@@ -0,0 +1,36 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var objects;
+(function (objects) {
+    var Bullet = /** @class */ (function (_super) {
+        __extends(Bullet, _super);
+        function Bullet() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        //private instance members
+        // public properties
+        // constructors
+        //private methods
+        Bullet.prototype.Start = function () {
+        };
+        Bullet.prototype.Update = function () {
+        };
+        Bullet.prototype.Reset = function () {
+        };
+        return Bullet;
+    }(objects.GameObject));
+    objects.Bullet = Bullet;
+})(objects || (objects = {}));
+//# sourceMappingURL=Bullet.js.map
